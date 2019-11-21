af5c856cb6051092d7b947b81f3b4237adf297a5
Dani DeSalvio <danidvisuals@gmail.com>
2019-11-20 17:43:14 -0700

    final file update

--- high-low.rb
+++ high-low.rb
@@ -1,196 +1,108 @@
 require 'colorize'
 
 require_relative 'player'
-# require_relative 'slots'
-# require_relative 'craps'
+require_relative 'slots'
+require_relative 'craps'
 
-class High_low  
-  attr_accessor :amount
-    def initialize(amount)
-      @amount = amount
+
+class HighLow
+	attr_accessor :amount
+	def initialize(amount)
+    @amount = amount
+    @high_or_low = nil
+    @bet = nil
+  end
+
+  def win
+    @amount += @bet
+    puts "Congrats you win!"
+    puts "You won #{@bet} dollars."
+  end
+
+  def lose
+    @amount -= @bet
+    puts "You lose monies."
+    puts "You lost #{@bet} dollars."
+  end
+
+  def game_reset
+    @high_or_low = nil
+    @bet = nil
+    puts "Do you want to play again? yes/no"
+   
+    input_incorrect = true
+    while input_incorrect
+      user_input = gets.strip.downcase
+      if user_input == "yes" || user_input == "no"
+        input_incorrect = false
+      else
+         puts "Invalid, try again."
+      end
     end
 
-    def intro
-        puts "\n"
-        puts "\n"
-        puts "\n"
-        puts "\n"
-        puts "\n"
-        puts "\n"
-        file = File.open("ascii_high-low.txt")
-        banner = file.read
-        puts banner.blue
-        puts "\n"
-        puts "--------------------".red
-        puts "!!WELCOME TO HIGH LOW!!".green
-        puts "--------------------".red
-        puts "\n"
-        puts "Would you like to place a bet?(y/n)"
-        
-        choice = gets.strip.capitalize
-          if choice == "Y"
-            play
-          elsif choice == "N"
-            intro
-          else
-            puts "Incorrect choice"
-            intro
-          end
-     end
-     
-     def play
-          
-     end
-end
-
-
-
-
-class High_Low
-    attr_accessor : amount
-    def initialize(amount)
-        @amount = amount
+    if user_input == "yes"
+      play
+    else user_input == "no"
+      puts "See you later."
+      # route into Casino w/team
     end
-
-  class Card
-    RANKS = %w(2 3 4 5 6 7 8 9 10 J Q K A) 
-    SUITS = %w(Spade Heart Club Diamond)
-
-    attr_accessor : rank, suit
-
-    def initialize(id)
-      self.rank = RANKS[id % 13]
-      self.suit = SUITS[id % 4]
-    end 
   end
   
-  # class Deck
-  #   attr_accessor : cards
-  #   def initialize
-  #     #shuffle array and init each Card
-  #     self.cards = (0..51).to_a.shuffle.collect { |id| Card.new(id)}
-  #   end
-  # end
-  # class Array
-  #   #knuth-fisher-yates shuffle algorithm
-  #   def shuffle!
-  #     n = length
-  #     for i in 0...n
-  #       r = rand(n-i)+i
-  #       self[r], self[i] = self[i], self[r]
-  #     end
-  #     self
-  #   end
-  #   def shuffle
-  #     dup.shuffle!
-  #   end
-
-
-  # d = Deck.new
-  # d.cards.each do |card|
-  #   puts "#{card.rank} #{card.suit}"
-  # end
-
-  #     #create an array listing all 52 cards or do 2 - ace
-  #     # make variables of dealer card vs player card
-  #     #player_card = array.sample
-  #     #dealer_card = array.sample --> will randomize it
-
-  #   class Card
-  #     attr_reader :suit, :value
-  #       def initialize(suit, value)
-  #           @suit = suit
-  #           @value = value
-  #       end
-  #       def value
-  #           return 10 if ["J", "Q", "K"].include?(@value)
-  #           return 11 if @value == "A"
-  #       end
-  #       def to_s
-  #           "#{@value}-#{suit}"
-  #       end
-  #   end
-
-  #   class Deck
-  #         attr_reader :cards
-
-  #         def initialize
-  #           @cards = Deck.build_cards
-  #         end
-
-  #         def self.build_cards
-  #               cards = []
-  #               end
-  #               cards.shuffle
-  #         end
-  #   end
+	def play
+		cards_high_low = [
+      {card: 'A', value: 1},
+      {card: '2', value: 2},
+      {card: '3', value: 3},
+      {card: '4', value: 4},
+      {card: '5', value: 5},
+      {card: '6', value: 6},
+      {card: '7', value: 7},
+      {card: '8', value: 8},
+      {card: '9', value: 9},
+      {card: '10', value: 10},
+      {card: 'J', value: 11},
+      {card: 'Q', value: 12},
+      {card: 'K', value: 13}
+    ]
+      
+    first_card = cards_high_low.sample
+    second_card = cards_high_low.sample
     
-    puts "Pick a Card"
-      #player gets card from dealer - variable is player_card
-      #need the player_card variable
-
+    puts "How much do you want to bet?"
+    @bet = gets.strip.to_i
     
-      #user case choice of betting high or low, any other text is invalid and will go back to 'bet'
-      def bet
-        puts "Place your bet. You may choose High or Low."
-          puts "High"
-          puts "Low"
-          puts "Exit"
+    puts "Dealer draws #{first_card[:card]} which is worth #{first_card[:value]}"
+    
 
-          user_choice = gets.strip.to_s
-          case bet_choice
-            when "High"
-          end
-      end
-
-      #dealer flips next card, the result will either be higher or lower than player card
-      #need the dealer_card variable here
-
-      #bet is placed, results will be through game_result
-    def game_result
-      if high
-        @dealer_card > @player_card   
-        
-      elsif low
-        @dealer_card < @player_card
+    puts "Time to bet, High or Low?"
+    
+    high_low_validated = false
+    while !high_low_validated
+      @high_or_low = gets.strip.downcase
+      if @high_or_low == "high" || @high_or_low == "low"
+        high_low_validated = true
       else
-        puts "Try again."
-        game_result
+        puts "Please type either high or low"
       end
     end
     
+    puts "Dealer's second card drawn is #{second_card[:card]} which is worth #{second_card[:value]}"
+    
+    case true
+    when first_card[:value] == second_card[:value]
+      puts "Draw!"
+    when @high_or_low == "high" && first_card[:value] < second_card[:value]
+      win
+    when @high_or_low == "high" && first_card[:value] > second_card[:value]
+      lose
+    when @high_or_low == "low" && first_card[:value] > second_card[:value]
+      win
+    when @high_or_low == "low" && first_card[:value] < second_card[:value]
+      lose
+    end
+    game_reset
+	end
+end
 
-
-  #     @player_card = array.sample
-  #     @dealer_card = array.sample
-
-
-
-
-
-    # def play
-    #   if @amount == 0
-    #     puts "You must have money in order to play!"
-      
-    #   else
-    #       puts "How much do you want to bet?"
-    #         bet = gets.strip.to_i
-    #         if bet > @amount
-    #             puts "You cannot bet more than you have in your wallet!"
-    #         play
-
-    #         elsif bet <= 0
-    #           puts "You cannot bet a negative amount or nothing!"
-    #           play
-
-    #         else
-    #           puts "Choose a card, then guess a number between 0 and 24."
-    #           elsif
-                #   number guessed is higher
-                #     puts "You lose!"
-                # end   
-                # elsif
-                # number guessed is lower 
-    #   end
-
-
+high_low = HighLow.new(5000)
+high_low.play
